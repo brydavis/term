ccbcc18ecd3a805bc8c1f6eb3031f7b662d251bf
brydavis <bryan@stax.io>
2015-05-20 05:56:49 -0700

    auto commit

---
+++ main.go
@@ -0,0 +1,91 @@
+// Reference Code Sources:
+// ********************************
+// github.com/inatus/ssh-client-go
+// godoc.org/golang.org/x/crypto/ssh
+// golang-basic.blogspot.com/2014/06/step-by-step-guide-to-ssh-using-go.html
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/user"
+
+	"golang.org/x/crypto/ssh"
+)
+
+func main() {
+	pkey, err := getKeyFile()
+	if err != nil {
+		panic(err)
+	}
+
+	config := &ssh.ClientConfig{
+		User: "root",
+		Auth: []ssh.AuthMethod{ssh.PublicKeys(pkey)},
+	}
+
+	// Dial your ssh server.
+	conn, err := ssh.Dial("tcp", "127.0.0.1:22", config) // TODO: pass arguments
+	if err != nil {
+		log.Fatalf("unable to connect: %s", err)
+	}
+	defer conn.Close()
+
+	session, err := conn.NewSession()
+	if err != nil {
+		panic("Failed to create session: " + err.Error())
+	}
+	defer session.Close()
+
+	// Set IO
+	session.Stdout = os.Stdout
+	session.Stderr = os.Stderr
+	in, _ := session.StdinPipe()
+
+	// Set up terminal modes
+	modes := ssh.TerminalModes{
+		ssh.ECHO:          0,     // disable echoing
+		ssh.TTY_OP_ISPEED: 14400, // input speed = 14.4kbaud
+		ssh.TTY_OP_OSPEED: 14400, // output speed = 14.4kbaud
+	}
+
+	// Request pseudo terminal
+	if err := session.RequestPty("xterm", 80, 40, modes); err != nil {
+		log.Fatalf("request for pseudo terminal failed: %s", err)
+	}
+
+	// Start remote shell
+	if err := session.Shell(); err != nil {
+		log.Fatalf("failed to start shell: %s", err)
+	}
+
+	// Accepting commands
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		str, _ := reader.ReadString('\n')
+
+		fmt.Fprint(in, str)
+		if str != "exit" {
+			os.Exit(1)
+		}
+	}
+}
+
+func getKeyFile() (key ssh.Signer, err error) {
+	usr, _ := user.Current()
+	fmt.Println(usr.Username, usr.HomeDir)
+	file := usr.HomeDir + "/.ssh/id_rsa"
+	buf, err := ioutil.ReadFile(file)
+	if err != nil {
+		return
+	}
+	key, err = ssh.ParsePrivateKey(buf)
+	if err != nil {
+		return
+	}
+	return
+}
